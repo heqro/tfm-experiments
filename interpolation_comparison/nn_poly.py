3ba036a6d204494f099572bf87baac043625a7bf
Hector <heqromancer@gmail.com>
2024-01-26 20:36:25 +0100

    Add polynomial interpolant nn.Module

---
+++ interpolation_comparison/nn_poly.py
@@ -0,0 +1,19 @@
+import torch
+
+
+class PolynomialInterpolant(torch.nn.Module):
+    def __init__(self, degree: int = 1, coefs: list[float] = []):
+        super(PolynomialInterpolant, self).__init__()
+        if coefs != []:
+            if len(coefs) != degree + 1:
+                raise Exception(f"Coefficients vector of length ({len(coefs)})  \
+                        does not fit prescribed degree ({degree}).")
+            else:
+                self.coefs = torch.nn.Parameter(torch.tensor(coefs))
+        else:
+            self.coefs = torch.nn.Parameter(torch.rand(degree + 1))
+        self.powers = torch.arange(start=0, end=degree + 1)
+
+    def forward(self, x: torch.Tensor):
+        return torch.tensor([self.coefs @ (input ** self.powers)
+                             for input in x]).reshape_as(x)
