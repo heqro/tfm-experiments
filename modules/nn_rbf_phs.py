71ff8c9cc162878755d50c16d3e7bad7ecd6fc01
Hector <heqromancer@gmail.com>
2024-02-05 21:22:20 +0100

    Add n-dimensional RBF-PHS interpolant

--- modules/nn_rbf_phs.py
+++ modules/nn_rbf_phs.py
@@ -4,24 +4,31 @@ import torch
 class RBFInterpolant(torch.nn.Module):
     def __init__(self, k: int, centers: torch.Tensor, coefs: list[float] = []):
         super(RBFInterpolant, self).__init__()
-        self.coefs = \
-            torch.nn.Parameter((torch.rand_like(centers) - 0.5)) if coefs == [] \
-            else torch.nn.Parameter(torch.tensor(coefs))
+        if coefs == []:
+            self.coefs = torch.nn.Parameter(
+                (torch.rand(centers.shape[0]) - 0.5))
+        else:
+            if len(coefs) != centers.shape[0]:
+                raise Exception(
+                    f'Mismatched dimensions: coefs {len(coefs)} w.r.t. centers {centers.shape[0]}')
+            else:
+                torch.nn.Parameter(torch.tensor(coefs))
         self.exponent = 2 * k + 1
         self.centers = centers
 
     def forward(self, x: torch.Tensor):
-        input_vector = x.view(-1, 1)
-        result = self.coefs @ (torch.abs(input_vector - self.centers)
-                               ** self.exponent).t()
-        return result.view_as(x)
+        return (self.coefs @ (torch.norm(x[:, None, :] - self.centers, dim=-1, p=2.0)
+                              ** self.exponent).t()).reshape([x.shape[0], 1])
 
-    def get_interpolation_matrix(self, *args):
+    def get_interpolation_matrix(self, *args):  # TODO REVISE
         auxiliary_vector = self.centers.view(-1, 1)
         return torch.abs(auxiliary_vector - self.centers) ** self.exponent
 
-    def get_coefs(self):
+    def get_coefs(self):  # TODO REVISE
         return self.coefs
 
-    def set_coefs(self, coefs: torch.Tensor | list[float]):
+    def set_coefs(self, coefs: torch.Tensor | list[float]):  # TODO REVISE
+        if len(coefs) != self.centers.shape[0]:
+            raise Exception(
+                f'Mismatched dimensions: coefs {len(coefs)} w.r.t. centers {self.centers.shape[0]}')
         self.coefs = torch.nn.Parameter(torch.tensor(coefs))
