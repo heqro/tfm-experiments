32f3d9e0db94e756dcdbb68592c6b7b36a7647c5
Hector <heqromancer@gmail.com>
2024-05-23 19:31:36 +0200

    Add circle domain geometry

--- modules/grid_utils.py
+++ modules/grid_utils.py
@@ -16,3 +16,27 @@ def cheb_points(num: int):
     arg_cos = torch.pi * (2 * naturals_list + 1)/(2 * num)
     # take opposite to return sorted list
     return -torch.cos(arg_cos)
+
+
+# assume (0,0) is the center
+def get_ball(radius: float, n_interior_points: int, n_boundary_points: int, dim=2):
+    def get_boundary_points(radius: float, n_points: int) -> torch.Tensor:
+        from torch import pi, cos, sin
+        points = torch.zeros(size=(n_points, dim))
+        angles = pi * torch.rand(n_points)
+        points[:, 0] = radius * cos(angles)
+        points[:, 1] = radius * sin(angles)
+        return points
+
+    def get_interior_points(xmin: float, xmax: float, radius: float, n_points: int) -> torch.Tensor:
+        inside_points = 0
+        points = torch.zeros(size=(n_points, dim))
+        while inside_points < n_points:
+            candidate = ((xmax - xmin) * torch.rand(1, 2) +
+                         xmin).reshape(-1, dim)
+            if torch.linalg.norm(candidate) < radius:
+                points[inside_points, :] = candidate
+                inside_points += 1
+        return points
+    return get_interior_points(-radius, radius, radius, n_interior_points), \
+        get_boundary_points(radius, n_boundary_points)
